c81b7c970cb50eb351bb0c97a273b27909599053
Hyung Mok Kim <hyungmokkim@Hyungs-MacBook-Pro.local>
2019-11-11 22:58:38 +0900

    WECODE 101 Mock up Data Created to simulate api fetch, Styled Components implemented

---
+++ src/AppComponents/ClassListDetailComponent/ClassAvailableFuture/ClassAvailableFuture.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components/native";
+
+const ClassAvailableFutureText = styled.Text`
+  margin-top: 10;
+  font-size: 9;
+  font-weight: 600;
+  background-color: #f8f8f9;
+  width: 60;
+  height: 20;
+  text-align-vertical: center;
+  text-align: center;
+  border-radius: 3;
+  color: ${props => props.color || "#fd7e14"};
+`;
+
+const ClassAvailableFuture = ({ children }) => {
+  return <ClassAvailableFutureText>{children}</ClassAvailableFutureText>;
+};
+
+export default ClassAvailableFuture;
---
+++ src/AppScreens/ClassListScreen/CollectionsMockData.js
@@ -0,0 +1,94 @@
+const collectionData = [
+  {
+    id: "0",
+    title: "실로 엮는 따뜻한 시간",
+    description: "기초부터 시작해서 스웨터, 뜨개인형을 만들어봐요",
+    classes: [
+      {
+        id: "1",
+        imageUrl:
+          "https://cdn.class101.net/images/e75739ae-85d0-42f0-8496-2a28226c7a43/320xauto",
+        title:
+          "스마트스토어로 월 100만원 만들기, 평범한 사람이 돈을 만드는 비법",
+        categoryCreater: "커리어 ・ 신사임당",
+        likeCount: "1000",
+        rating: "99%"
+      },
+      {
+        id: "2",
+        imageUrl:
+          "https://cdn.class101.net/images/274799d9-cfcf-4b74-9e09-d8b28b53fb0a/320xauto",
+        title: "코바늘로 인형을 만들자! 시은맘의 꼼지락 작업실 안녕하세요",
+        categoryCreater: "By. 시은맘",
+        likeCount: "1001",
+        rating: "99%"
+      },
+      {
+        id: "3",
+        imageUrl:
+          "https://cdn.class101.net/images/e75739ae-85d0-42f0-8496-2a28226c7a43/320xauto",
+        title:
+          "스마트스토어로 월 100만원 만들기, 평범한 사람이 돈을 만드는 비법",
+        categoryCreater: "커리어 ・ 신사임당",
+        likeCount: "1002",
+        rating: "99%"
+      },
+      {
+        id: "4",
+        imageUrl:
+          "https://cdn.class101.net/images/274799d9-cfcf-4b74-9e09-d8b28b53fb0a/320xauto",
+        title: "코바늘로 인형을 만들자! 시은맘의 꼼지락 작업실 안녕하세요",
+        categoryCreater: "By. 시은맘",
+        likeCount: "1003",
+        rating: "99%"
+      }
+    ]
+  },
+  {
+    id: "1",
+    title: "실로 엮는 따뜻한 시간222",
+    description: "기초부터 시작해서 스웨터, 뜨개인형을 만들어봐요222",
+    classes: [
+      {
+        id: "1",
+        imageUrl:
+          "https://cdn.class101.net/images/e75739ae-85d0-42f0-8496-2a28226c7a43/320xauto",
+        title:
+          "스마트스토어로 월 100만원 만들기, 평범한 사람이 돈을 만드는 비법",
+        categoryCreater: "커리어 ・ 신사임당",
+        likeCount: "1000",
+        rating: "99%"
+      },
+      {
+        id: "2",
+        imageUrl:
+          "https://cdn.class101.net/images/274799d9-cfcf-4b74-9e09-d8b28b53fb0a/320xauto",
+        title: "코바늘로 인형을 만들자! 시은맘의 꼼지락 작업실 안녕하세요",
+        categoryCreater: "By. 시은맘",
+        likeCount: "1001",
+        rating: "99%"
+      },
+      {
+        id: "3",
+        imageUrl:
+          "https://cdn.class101.net/images/e75739ae-85d0-42f0-8496-2a28226c7a43/320xauto",
+        title:
+          "스마트스토어로 월 100만원 만들기, 평범한 사람이 돈을 만드는 비법",
+        categoryCreater: "커리어 ・ 신사임당",
+        likeCount: "1002",
+        rating: "99%"
+      },
+      {
+        id: "4",
+        imageUrl:
+          "https://cdn.class101.net/images/274799d9-cfcf-4b74-9e09-d8b28b53fb0a/320xauto",
+        title: "코바늘로 인형을 만들자! 시은맘의 꼼지락 작업실 안녕하세요",
+        categoryCreater: "By. 시은맘",
+        likeCount: "1003",
+        rating: "99%"
+      }
+    ]
+  }
+];
+
+export default collectionData;
